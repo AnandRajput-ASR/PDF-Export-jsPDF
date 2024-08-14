aa14583dc2ae4ca1f03b8c62cc9b02d0ded477e7
Anand Rajput <Anandraj.asr@gmail.com>
2024-08-14 22:07:40 +0530

    app component changes with navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PdfService } from './services/pdf.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,34 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PDF-Export-jsPDF';
+  translations: any;
+  constructor(private PDFService: PdfService){
+  }
+
+  ngOnInit(): void {
+    // Mock API response for translations
+    this.translations = {
+      en: { content: 'Hello, this is a translated text in English!' },
+      fr: { content: 'Bonjour, ceci est un texte traduit en français!' }
+    };
+  }
+
+  downloadPDF(){
+    this.PDFService.generatePDF();
+  }
+  exportPdf(contentKey: string, language: string): void {
+    const fontName = 'chilispepper';
+    const fontFile = 'chilispepper.ttf';
+    const fontType: 'ttf' | 'base64' = 'ttf';
+    this.PDFService.generatePdfNew(contentKey, language, this.translations, fontName, fontFile, fontType);
+  }
+
+  downloadPDF1(contentKey: string, language: string){
+    const fontName = 'chilispepper';
+    const fontFile = 'chilispepper.ttf';
+    const fontType: 'ttf' | 'base64' = 'ttf';
+    this.PDFService.generatePDF1(contentKey, language, this.translations, fontName, fontFile, fontType);
+  }
 }
